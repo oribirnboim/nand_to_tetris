4b8bfffe1a9c3ea227f50515389c5c6e283b298a
oribirnboim <ori.birnboim.44@gmail.com>
2024-09-29 23:58:35 +0300

    finished 06. only tested without L in assembler. no shift

--- projects/06/Code.py
+++ projects/06/Code.py
@@ -9,7 +9,7 @@ Unported [License](https://creativecommons.org/licenses/by-nc-sa/3.0/).
 
 class Code:
     """Translates Hack assembly language mnemonics into binary codes."""
-    
+
     @staticmethod
     def dest(mnemonic: str) -> str:
         """
@@ -20,7 +20,22 @@ class Code:
             str: 3-bit long binary code of the given mnemonic.
         """
         # Your code goes here!
-        pass
+        dest_dict = {'': '000',
+            'M': '001',
+            'D': '010',
+            'MD': '011',
+            'A': '100',
+            'AM': '101',
+            'AD': '110',
+            'AMD': '111'}
+        return dest_dict[mnemonic]
+
+    @staticmethod
+    def binary(num: int) -> str:
+        res = bin(num)
+        res = res[2:]
+        if len(res) > 15: return res[-15:]
+        else: return (15 - len(res))*"0" + res
 
     @staticmethod
     def comp(mnemonic: str) -> str:
@@ -32,7 +47,35 @@ class Code:
             str: the binary code of the given mnemonic.
         """
         # Your code goes here!
-        pass
+        comp_dict = {'0': '0101010',
+            '1': '0111111',
+            '-1': '0111010',
+            'D': '0001100',
+            'A': '0110000',
+            'M': '1110000',
+            '!D': '0001101',
+            '!A': '0110001',
+            '!M': '1110001',
+            '-D': '0001111',
+            '-A': '0110011',
+            '-M': '1110011',
+            'D+1': '0011111',
+            'A+1': '0110111',
+            'M+1': '1110111',
+            'D-1': '0001110',
+            'A-1': '0110010',
+            'M-1': '1110010',
+            'D+A': '0000010',
+            'D+M': '1000010',
+            'D-A': '0010011',
+            'D-M': '1010011',
+            'A-D': '0000111',
+            'M-D': '1000111',
+            'D&A': '0000000',
+            'D&M': '1000000',
+            'D|A': '0010101',
+            'D|M': '1010101'}
+        return comp_dict[mnemonic]
 
     @staticmethod
     def jump(mnemonic: str) -> str:
@@ -44,4 +87,12 @@ class Code:
             str: 3-bit long binary code of the given mnemonic.
         """
         # Your code goes here!
-        pass
+        jump_dict = {'': '000',
+            'JGT': '001',
+            'JEQ': '010',
+            'JGE': '011',
+            'JLT': '100',
+            'JNE': '101',
+            'JLE': '110',
+            'JMP': '111'}
+        return jump_dict[mnemonic]
